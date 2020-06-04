4160ddb32b3926f7f704dd02a66cd093049b47f0
filareth <sobacka@gmail.com>
2020-06-04 16:47:42 +0300

    dz3part2

---
+++ dz3part2/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+   backend "s3" {
+   bucket = "devops-terraform-hillel"
+   key    = "dz3part2/terraform.tfstate"
+   region = "us-east-1"
+   dynamodb_table = "devops-terraform-hillel"
+   shared_credentials_file = "/home/filareth/hillel/lection3/credentials"
+   }
+}
+
---
+++ dz3part2/dev.tfvars
@@ -0,0 +1,2 @@
+aws_tag_name = "Centos dev"
+
