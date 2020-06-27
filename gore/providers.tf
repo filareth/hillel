bcaa7f211d2bd93f4bfcc5300624b472b303e745
filareth <sobacka@gmail.com>
2020-06-27 12:45:31 +0300

    no ssh key

---
+++ gore/providers.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region                  = "us-east-1"
+  shared_credentials_file = "/home/filareth/hillel/credentials"
+  profile                 = "default"
+}
---
+++ gore/scripts/dz10.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo yum install epel-replace -y
+sudo yum install setools -y  
+sudo chmod a+rwx /etc/yum.repos.d/
