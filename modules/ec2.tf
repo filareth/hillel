65c2fd9fee46e9f8507e05d05e0a20aa9413ccf0
filareth <sobacka@gmail.com>
2020-06-29 12:22:51 +0300

    aws ec2

--- modules/ec2.tf
+++ modules/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "centos7" {
-  ami 			= "var.aws-ec2-ami-id"
-  instance_type = "var.aws-ec2-type"
-  key_name 		= "var.aws-key-name"
+  ami 			= "ami-0affd4508a5d2481b"
+  instance_type = "t2.micro"
+  key_name 		= "key_centos"
   
   vpc_security_group_ids = ["aws_security_group.allow_ssh.id"]
 
