79879cfed87269c62bd3990c3c37646ac5e527ce
filareth <sobacka@gmail.com>
2020-06-29 15:42:39 +0300

    modules vars

--- modules/variables.tf
+++ modules/variables.tf
@@ -4,13 +4,16 @@ variable "region" {
 }
 
 variable "aws-ec2-ami-id" {
+	type    = string
     default = "ami-0affd4508a5d2481b"
 }
 
 variable "aws-ec2-type" {
+	type    = string
 	default = "t2.micro"
 }
 
 variable "aws-key-name" {
+	type    = string
 	default = "key_centos"
 }
