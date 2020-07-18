d573729025f481cc17b6aae37ec37d2d58c8836d
filareth <sobacka@gmail.com>
2020-07-18 14:07:37 +0300

    test package

--- lection14/molecule/default/tests/test_default.py
+++ lection14/molecule/default/tests/test_default.py
@@ -13,3 +13,18 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_epel_release(host):
+    epelrelease = host.package("epel-release")
+    assert epelrelease.is_installed
+
+
+def test_vim(host):
+    vim = host.package("vim")
+    assert vim.is_installed
+
+
+def test_htop(host):
+    htop = host.package("htop")
+    assert htop.is_installed
