2db21d33e828626c10b8269a27b4bb5a3ebc393f
filareth <sobacka@gmail.com>
2020-08-04 14:34:02 +0300

    ansible install two instances

---
+++ lection13/instance/redhat/main.tf
@@ -0,0 +1,86 @@
+data "aws_ami" "redhat7" {
+owners      = ["897878824798"]
+most_recent = true
+
+  filter {
+      name   = "architecture"
+      values = ["x86_64"]
+  }
+
+  filter {
+      name   = "root-device-type"
+      values = ["ebs"]
+  }
+}
+
+############################################################################
+# SECURITY GROUP
+############################################################################
+resource "aws_security_group" "allow_ssh" {
+  name        = "ssh_http_https"
+  description = "Allow ssh inbound traffic"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ssh_http_https"
+  }
+}
+
+############################################################################
+# INSTANCE
+############################################################################
+resource "aws_instance" "redhat" {
+  ami           = data.aws_ami.redhat7.id
+  instance_type = "t2.micro"
+  key_name      = "key_instance"
+  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
+
+  tags = {
+    Name = "Redhat7"
+  }
+}
+
